d5abc97fcdca2e604786d2b2a12fba1592053b62
karjalane <nino.karjalainen@gmail.com>
2019-11-06 21:02:39 +0200

    Phonebook ja db step 6
    Tehtävät 3.15 - 3.18

--- osa3/puhelinbackend/index.js
+++ osa3/puhelinbackend/index.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const cors = require('cors')
+
 const Contact = require('./models/contact')
 
 app.use(bodyParser.json())
+const cors = require('cors')
+
 app.use(cors())
-app.use(express.static('build'))
 
 // Morgan spec
 morgan.token('body', (req, res) => {
@@ -45,45 +46,14 @@ let contacts = [
 ]
 */
 
+app.use(express.static('build'))
+
 // Get base url
 app.get('/', (req, res) => {
     res.send('<h1>poop</h1>')
 })
 
-// Get all contacts
-app.get('/api/persons', (req, res) => {
-    Contact.find( {} ).then(contacts => {
-        res.json(contacts.map(contact => contact.toJSON()))
-    })
-})
-
-// Get single contact
-app.get('/api/persons/:id', (req, res) => {
-    Contact.findById(req.params.id).then(contact => {
-        res.json(contact.toJSON())
-    })
-})
-
-// Get info
-app.get('/info', (req, res) => {
-    const amount = contacts.length
-    const date = new Date()
-
-    res.send(`
-        <div>Phonebook has info for ${amount} people</div>
-        <br />
-        <div>${date}</div>`)
-})
-
-// Delete single contact
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    contacts = contacts.filter(c => c.id !== id)
-    
-    res.status(204).end()
-})
-
-const checkID = ( ID ) => {
+/*const checkID = ( ID ) => {
     const ind = contacts.findIndex(x =>
         x.id === ID)
     if (ind >= 0) {
@@ -111,14 +81,14 @@ const checkContact = ( value ) => {
     if (ind >= 0) {
         return true
     } return false
-}
+}*/
 
 // Add new contact
 app.post('/api/persons', (req, res) => {
     const body = req.body
     //const id = generateID()
 
-    if (!body.name) {
+    /*if (!body.name) {
         return res.status(400).json({
             error: 'Name missing'
         })
@@ -128,7 +98,7 @@ app.post('/api/persons', (req, res) => {
             error: 'Number missing'
         })
     }
-    /*if (id < 0) {
+    if (id < 0) {
         return res.status(400).json({
             error: 'Database full'
         })
@@ -148,6 +118,80 @@ app.post('/api/persons', (req, res) => {
         res.json(savedContact.toJSON())
     })
 })
+
+// Get all contacts
+app.get('/api/persons', (req, res) => {
+    Contact.find( {} ).then(contacts => {
+        res.json(contacts.map(contact => contact.toJSON()))
+    })
+})
+
+// Get single contact
+app.get('/api/persons/:id', (req, res, next) => {
+    Contact.findById(req.params.id)
+        .then(contact => {
+            if (contact) {
+                res.json(contact.toJSON())
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const contact = {
+        name: body.name,
+        number: body.number
+    }
+
+    Contact.findByIdAndUpdate(req.params.id, contact, { new: true })
+        .then(updatedContact => {
+            res.json(updatedContact.toJSON())
+        })
+        .catch(error => next(error))
+})
+
+// Delete single contact
+app.delete('/api/persons/:id', (req, res, next) => {
+    Contact.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+// Get info
+app.get('/info', (req, res) => {
+    const date = new Date()
+    
+    Contact.countDocuments().then((a) => {
+        res.status(200).send(
+            `<div>Phonebook has info for ${a} people</div>
+            <br />
+            <div>${date}</div>`)
+    })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'Malformatted ID' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
